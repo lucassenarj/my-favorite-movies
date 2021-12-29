0542ddf7a27c9d3b158a52a4bec47a4c1d123e91
Lucas Sena <lucassena.rj@gmail.com>
2021-12-29 22:41:42 +0000

    Updates

---
+++ static/js/901.fae47529.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_favorite_movies=self.webpackChunkmy_favorite_movies||[]).push([[901],{901:function(e,r,t){t.r(r);var n=t(2791),s=t(184),o=n.lazy((function(){return Promise.resolve().then(t.bind(t,7981))}));r.default=function(e){var r=e.resource.recommendations.read().data.results;return(0,s.jsx)(o,{title:"Recommendations",movies:r})}}}]);
+//# sourceMappingURL=901.fae47529.chunk.js.map
